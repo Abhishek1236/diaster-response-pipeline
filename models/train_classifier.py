720e4d904f129ae66c069d8e338d3ce9f846e20d
Abhishek ravi <abhishekravi60@gmail.com>
2020-05-21 21:18:53 +0530

    updated train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -126,10 +126,7 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+        print('Read the documentation about running the models')
 
 
 if __name__ == '__main__':
